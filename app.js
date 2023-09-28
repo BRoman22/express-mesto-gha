55c015e943e1fc388689c5a51dc17541259c2b8a
Roman Bondarev <Rbondarev22@gmail.com>
2023-09-28 22:20:37 +0700

    fix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
-import "dotenv/config";
+//import "dotenv/config";
 import userRouter from "./routes/users.js";
 import cardRouter from "./routes/cards.js";
 
-const { PORT, MONGO_URL } = process.env;
+const { PORT = 3000, MONGO_URL = "mongodb://127.0.0.1:27017/mestodb" } =
+  process.env;
 
 const app = express();
 
