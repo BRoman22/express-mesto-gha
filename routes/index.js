a14a00594572e74cb9693e6f54ec3e884210aab7
Roman Bondarev <Rbondarev22@gmail.com>
2023-09-30 11:33:50 +0700

    исправлены ошибки по ревью

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import userRouter from './users';
 import cardRouter from './cards';
+import { NOT_FOUND } from '../utils/statusCodes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
 routes.use('/cards', cardRouter);
-routes.use('*', (req, res) => res.status(404)
+routes.use('*', (req, res) => res.status(NOT_FOUND)
   .send({ message: 'Такой ресурс еще не создан' }));
 
 export default routes;
