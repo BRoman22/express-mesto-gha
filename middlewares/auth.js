42e06b926ba98cbb3d860146a3ba498c433080c4
Roman Bondarev <Rbondarev22@gmail.com>
2023-10-11 11:43:52 +0700

    test gha

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,23 +4,21 @@ import Unauthorized from '../errors/Unauthorized';
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  const authorization = req.cookies.jwtKey;
-  if (!authorization) {
-    return next(new Unauthorized('Необходима авторизация'));
-  }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'super-strong-secret');
-  } catch (err) {
-    return next(new Unauthorized('Необходима авторизация'));
+    const { authorization } = req.headers;
+    const { cookies } = req;
+    if ((authorization && authorization.startsWith('Bearer ')) || (cookies && cookies.jwtKey)) {
+      const token = authorization ? authorization.replace('Bearer ', '') : cookies.jwtKey;
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+      req.user = payload;
+      next();
+    } else {
+      next(new Unauthorized('Необходима авторизация'));
+    }
+  } catch (error) {
+    next(new Unauthorized('Необходима авторизация'));
   }
-
-  req.user = payload;
-
-  return next();
 };
 
 export default auth;
