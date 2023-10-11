f6936d39bf9fac1619d8397a54cba330f6ed0ca6
Roman Bondarev <Rbondarev22@gmail.com>
2023-10-11 16:31:06 +0700

    test gha

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,13 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 export default function auth(req, res, next) {
   const { jwtKey } = req.cookies;
+  const { authorization } = req.headers;
 
-  if ((!jwtKey)) {
+  if ((!jwtKey) && (!authorization && !authorization.startsWith('Bearer '))) {
     return next(new Unauthorized('Необходима авторизация'));
   }
 
-  // const token = jwtKey.replace('Bearer ', '');
-  const token = jwtKey;
+  const token = jwtKey || authorization.replace('Bearer ', '');
   const secret = NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret';
   let payload;
 
