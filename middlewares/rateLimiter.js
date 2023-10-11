be926b390aa9e18eaec53aaeac2d9307d2dfe9af
Roman Bondarev <Rbondarev22@gmail.com>
2023-10-12 00:29:42 +0700

    fix agter review

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,8 +2,8 @@ import { rateLimit } from 'express-rate-limit';
 import TooManyRequests from '../errors/TooManyRequests';
 
 const limiter = rateLimit({
-  limit: 153,
-  windowMS: 3 * 60 * 1000, // 3 minutes
+  limit: 160,
+  windowMS: 15 * 60 * 1000, // 15 minutes
   handler: (req, res, next) => next(new TooManyRequests(
     'Слишком много запросов, пожалуйста, повторите попытку позже',
   )),
