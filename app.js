d297bd109bca19b4d963e7bc4a51e06bbffaff38
Roman Bondarev <Rbondarev22@gmail.com>
2023-10-11 19:35:02 +0700

    test gha

--- app.js
+++ app.js
@@ -31,4 +31,4 @@ mongoose
     app.listen(PORT);
     console.log(`App listening on port ${PORT}`);
   })
-  .catch(() => console.log('Mongo don`t connect'));
+  .catch(() => console.log('Mongo dont connect'));
